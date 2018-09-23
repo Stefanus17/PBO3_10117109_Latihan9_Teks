9ec4b62641eea7562fac9cd7afa1018cd3544217
Stefanus17 <43497735+Stefanus17@users.noreply.github.com>
2018-09-23 19:06:17 +0700

    Add files via upload

---
+++ PBO3_10117109_Latihan9_Teks.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117109_latihan9_teks;
+
+/**
+ *
+ * @author
+ * NAMA  : Stefanus Saputra
+ * KELAS : IF 3
+ * NIM   : 10117109
+ */
+public class PBO3_10117109_Latihan9_Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c );
+        
+    }
+    
+}
